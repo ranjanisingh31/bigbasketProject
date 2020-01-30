a78fc30539f53f9b434314f7d4df865e176b03ec
Ranjani_Singh <ranjanisingh312000@gmail.com>
2020-01-30 19:24:40 +0530

    Modified map operations

--- HashMap/map.js
+++ HashMap/map.js
@@ -1,80 +1,53 @@
-
-
-
 let m = new Map();
 
 //Adding elements in map
-//var a=document.getElementById("buttonAdd");
 function addValue() {
   console.log(1);
-  var k=prompt("key=");
-  var v=prompt("value=");
-  if(m.set(k, v))
-  { 
-      alert("key " + k + " and value " + v + " pair added in map");
-  } 
-  else {
+  var k = prompt("key=");
+  var v = prompt("value=");
+  if (m.set(k, v)) {
+    alert("key " + k + " and value " + v + " pair added in map");
+  } else {
     alert("key " + k + " and value " + v + " pair not added in map");
   }
-
+  display();
 }
-
-
 
 //deleting key-value pair in map
 function deleteValue() {
-  var d=prompt("Enter key to be deleted?");
-  if(m.delete(d))
-  {
-       alert("key " + k + " and value " + v " pair deleted in map");
-  } 
-  else {
-    alert("key " + k + " and value " + v + " pair not found in map");
+  var d = prompt("Enter key to be deleted?");
+  if (m.delete(d)) {
+    alert("key " + d + " and value " + m.get(d) + " pair deleted in map");
+  } else {
+    alert("key " + d + " and value " + m.get(d) + " pair not found in map");
   }
+  console.log(1);
   display();
-  }
-
+}
 
 //searching key-value pair in map
 function searchValue() {
-  var s=prompt("Enter key to be searched?");
+  var s = prompt("Enter key to be searched?");
   if (m.has(s)) {
-    alert("key " + k + " and value " + v+" pair found in map");
+    alert("pair found in map");
   } else {
-    alert("key " + k + " and value " + v + " pair not found in map");
+    alert("pair not found in map");
   }
 }
 
 //display all pairs of map
-/*function display() {
-  var e = "</br>";
+function display() {
+  var e = "key &nbsp value</br>";
   for (var [key, value] of m.entries()) {
-    e += key + " " + value + "</br>";
+    e += key + "    " + value + "</br>";
   }
   document.getElementById("para").innerHTML = e;
-}*/
+}
 
 //clear all
 function clearValue() {
-    if(m.clear())
-    {
-         alert("cleared");
-  } 
-  else {
-    alert("already cleared");
-  }
+  m.clear();
+  alert("cleared");
 
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
+  display();
+}
